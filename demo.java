eb4f4789b8f5df4fb801d19b7a9aa49b9da40838
neha sirsat <nehasirsat2000@gmail.com>
2024-07-30 19:53:00 +0530

    java file

---
+++ demo.java
@@ -0,0 +1,10 @@
+
+import java.util.*;
+class demo{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter n");
+        int n=sc.nextInt();
+        System.out.println("n is"+n);
+    }
+}
